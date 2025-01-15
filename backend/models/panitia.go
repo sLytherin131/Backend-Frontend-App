7f60d885856c58825b5eb218af53e1637e18855e
Ricky Junianto Wijaya <86522702+sLytherin131@users.noreply.github.com>
2025-01-16 04:07:52 +0700

    Delete backend directory

--- backend/models/panitia.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Panitia struct {
-	IdPanitia uint   `gorm:"primaryKey"`
-	Nama      string `gorm:"not null"`
-	Email     string `gorm:"unique;not null"`
-	Jabatan   string
-}
--- backend/routes/routes.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-import "github.com/gofiber/fiber/v2"
-
-func RegisterRoutes(app *fiber.App) {
-	RegisterPanitiaRoutes(app)
-	RegisterTugasPanitiaRoutes(app)
-	RegisterJadwalWebinarRoutes(app)
-}
